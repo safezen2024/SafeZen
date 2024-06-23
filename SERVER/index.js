101ce49587081d3b5efc25ffd0d4bdb42ee9d4f3
jinamsancheti <jinamsancheti2@gmail.com>
2024-06-23 15:25:38 +0530

    updating Cors Policy try 14

--- SERVER/index.js
+++ SERVER/index.js
@@ -45,7 +45,7 @@ app.use(
 );
 
 app.use(function (req, res, next) {
-	res.setHeader("Access-Control-Allow-Origin", "https://safezen.in, https://www.safezen.in");
+	res.setHeader("Access-Control-Allow-Origin", "https://safezen.in");
 	res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
 	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers");
 	next();
