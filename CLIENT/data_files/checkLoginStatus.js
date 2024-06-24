4aaf8814e7f95b80312d2330376e37743e8747b1
jinamsancheti <jinamsancheti2@gmail.com>
2024-06-24 23:20:58 +0530

    Updating client side 1

--- CLIENT/data_files/checkLoginStatus.js
+++ CLIENT/data_files/checkLoginStatus.js
@@ -1,70 +1,14 @@
-// import Cookies from "js-cookie";
-// import axios from "axios";
-
-// export let auth = false;
-// export let email = "";
-// axios.defaults.withCredentials = true;
-
-// try {
-// 	let token = Cookies.get("token");
-// 	console.log(token);
-// 	if (token) {
-// 		if (token[0] === "j" && token[1] == ":") {
-// 			// Retrieve the token from the cookie
-// 			if (token) {
-// 				token = token.slice(2);
-// 				token = JSON.parse(token);
-// 				console.log("token yeh hai: ", token);
-// 				if (token) {
-// 					axios
-// 						.post("https://safezen.onrender.com/verifyToken", { token })
-// 						.then((res) => {
-// 							if (res.data.Status === "Success") {
-// 								console.log("abcxyz");
-// 								auth = true;
-// 								email = res.data.email;
-// 							} else {
-// 								auth = false;
-// 								console.log(res.data.Error);
-// 							}
-// 						})
-// 						.catch((err) => console.error("Error verifying token:", err));
-// 				} else {
-// 					auth = false;
-// 				}
-// 			}
-// 		} else {
-// 			try {
-// 				axios
-// 					.get("https://safezen.onrender.com")
-// 					.then((res) => {
-// 						if (res.data.Status === "Success") {
-// 							auth = true;
-// 							email = res.data.email;
-// 						} else {
-// 							auth = false;
-// 							setMsg(res.data.Error);
-// 						}
-// 					})
-// 					.catch((err) => console.log(err));
-// 			} catch (err) {
-// 				console.error(err.message);
-// 			}
-// 		}
-// 	}
-// } catch (err) {
-// 	console.error("Error geetting token:", err);
-// }
-
 import Cookies from "js-cookie";
 import axios from "axios";
+import { useCookies } from 'react-cookie';
 
 export let auth = false;
 export let email = "";
 axios.defaults.withCredentials = true;
 
 try {
-	let token = Cookies.get("token");
+	// let token = Cookies.get("token");
+	let token = useCookies['token'];
 	// let token = document.cookie;
 	console.log("Retrieved token:", token); // Debugging log
 
