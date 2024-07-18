d4a4d9f811e61ce2c13ea88d8d99cf197d139284
jinamsancheti <jinamsancheti2@gmail.com>
2024-07-19 01:44:45 +0530

    Determined to make cookies work 7

--- SERVER/index.js
+++ SERVER/index.js
@@ -68,7 +68,7 @@ app.use((req, res, next) => {
 });
 
 app.use(function (req, res, next) {
-	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Origin", "https://safezen.in");
 	res.setHeader("Access-Control-Allow-Methods", "*");
 	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers");
 	next();
